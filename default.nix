4d04162951b89dbfcaaba95b84567bf009035387
Splinter Suidman <splinter@mannenopdemaan.nl>
2019-05-23 17:00:44 +0200

    Update nix configuration.

--- default.nix
+++ default.nix
@@ -1,11 +1,2 @@
-{ mkDerivation, base, containers, HUnit, stdenv, text }:
-mkDerivation {
-  pname = "Comb";
-  version = "0.2.0.0";
-  src = ./.;
-  libraryHaskellDepends = [ base containers text ];
-  testHaskellDepends = [ base HUnit ];
-  homepage = "https://github.com/splintah/Comb";
-  description = "Parser combinator library";
-  license = stdenv.lib.licenses.agpl3;
-}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865" }:
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./Comb.nix { }
