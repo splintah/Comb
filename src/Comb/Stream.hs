31c001294cadd8cd90e5842375ce8c5de0e9db7a
Splinter Suidman <splinter@mannenopdemaan.nl>
2019-05-23 18:27:33 +0200

    Remove unused prototype from Comb.Stream.

--- src/Comb/Stream.hs
+++ src/Comb/Stream.hs
@@ -30,36 +30,3 @@ instance (Stream s t, UpdatePosition t) => Stream (WithPosition s) t where
     where
       f (t, s) =
         (t, WithPosition { position = updatePosition t position, stream = s })
-
--- -- | A stream 's' with symbol 's'.
--- class Stream s where
---   -- This approach allows for exactly one symbol type per stream type. If we had
---   -- used MultiParamTypeClasses, a stream could have two different symbols.
---   -- Which of these approaches is correct?
---   -- | The symbol which the stream contains.
---   type Symbol s :: *
---   -- | Update the position with the last-read character.
---   updatePos :: Position -> Symbol s -> Position
---   -- | Get the first symbol in the stream and the rest of the stream. If the
---   -- stream is empty, 'Nothing' must be returned.
---   uncons :: s -> Maybe (Symbol s, s)
-
--- updatePosChar :: Position -> Char -> Position
--- updatePosChar pos@Position { line, column } c =
---   case c of
---     '\n' -> pos { line = line + 1, column = 0 }
---     '\r' -> pos { column = 0 }
---     _ -> pos { column = column + 1 }
-
--- instance Stream String where
---   type Symbol String = Char
---   updatePos = updatePosChar
---   uncons cs =
---     case cs of
---       c:cs' -> Just (c, cs')
---       [] -> Nothing
-
--- instance Stream Text where
---   type Symbol Text = Char
---   updatePos = updatePosChar
---   uncons = Text.uncons
